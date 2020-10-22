2f74d8cfd58970e74c20bcb412ff1d48dec0b2ec
GATIS ZAGARS <gatis@klients.eu>
2020-10-22 15:05:13 +0300

    friendly exception message was added

--- SEF/src/sef/module8/activity/TryCatchFinallyActivity.java
+++ SEF/src/sef/module8/activity/TryCatchFinallyActivity.java
@@ -12,18 +12,21 @@ public static void main(String[] args) {
 void catchMeIfYouCan()
 {
 	int [] arr={0,1,2,3,4,5,6,7,8,9};
-	
+	try {
 	for(int i=0;i<=10;i++)
-	{
-		try {
+		{
+		
 		System.out.println(arr[i]);
-		}catch (Exception e) {
+			
+		}
+	
+	}catch (Exception e) {
+		System.out.println("The exception has occured");
 	}
-	
+	finally {
+		System.out.println("This should get printed even if there is an exception");
 	}
-	
-	System.out.println("This should get printed even if there is an exception");
-	
+	}
 }
 
-}
+
